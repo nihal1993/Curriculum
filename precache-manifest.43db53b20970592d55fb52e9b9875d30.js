50fd73f3c9c260e8f51f724bd3511d71e6426ee6
nihal1993 <jaimini.nihal@gmail.com>
2020-08-18 17:32:05 +0530

    Updates

---
+++ precache-manifest.43db53b20970592d55fb52e9b9875d30.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f18c6fbcc79a90507faaa0c8831851",
+    "url": "/Curriculum/index.html"
+  },
+  {
+    "revision": "4d5e4e55de64b1b4b0c2",
+    "url": "/Curriculum/static/css/2.9b1c0336.chunk.css"
+  },
+  {
+    "revision": "a3023db93280a2b980f5",
+    "url": "/Curriculum/static/css/main.fb885964.chunk.css"
+  },
+  {
+    "revision": "4d5e4e55de64b1b4b0c2",
+    "url": "/Curriculum/static/js/2.6c9da21e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Curriculum/static/js/2.6c9da21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3023db93280a2b980f5",
+    "url": "/Curriculum/static/js/main.1bd1e523.chunk.js"
+  },
+  {
+    "revision": "4f2400da541ae44e6f74",
+    "url": "/Curriculum/static/js/runtime-main.bd972e5c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Curriculum/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Curriculum/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Curriculum/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Curriculum/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Curriculum/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
