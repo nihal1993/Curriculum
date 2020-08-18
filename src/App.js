ed8bef92202e0c6c1f23a5094940a1d7442df66f
nihal1993 <jaimini.nihal@gmail.com>
2020-08-18 17:46:30 +0530

    add tab class

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
+import Tab from './Tab';
 const itemsArray = [];
 
-
-class Tab extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state = {
-      parent:false,
-      child:false,
-    }
-    this.leftItem = this.leftItem.bind(this);
-    this.rightItem = this.rightItem.bind(this);
-  }
-
-  leftItem(e){
-    console.log("left",e);
-    if(this.state.child === true){
-      this.setState({child: false});
-    }else{
-      if(this.state.parent === true){
-      this.setState({parent: false});
-    }}
-  }
-
-  rightItem(e){
-    console.log("right",e);
-    if(this.state.parent === true){
-      this.setState({child: true});
-    }else{
-      if(this.state.parent === false){
-       this.setState({parent: true});
-      } 
-    }
-  }
-
-  render() {
-    return(
-    <tr>
-      <td>
-        
-        <i onClick={this.props.upItem }  class="fa fa-arrow-circle-up	 fa-3x"></i>
-        <i onClick={this.props.downItem }  class="fa fa-arrow-circle-down fa-3x"></i>
-        <i onClick={(e)=>{this.leftItem(e)}} class="fa fa-arrow-circle-left fa-3x"></i>
-        <i onClick={(e)=>{this.rightItem(e)}}  class="fa fa-arrow-circle-right fa-3x"></i>
-        <i onClick={this.props.deleteItem } class="fa fa-trash-o fa-3x"></i>
-        
-      </td>
-      <td className={ `list-item ${this.state.parent ? "shiftInside" : ""} ${this.state.child ? "shiftDeepInside" : ""}` }>   { this.props.content }
-      </td>
-   </tr>);
-  }
-}
 
 class ItemList extends React.Component {
   constructor(props){
@@ -166,18 +116,6 @@ return (
               deleteItem={() => this.deleteItem(item)}
             />
               
-            //   <tr>
-            //   <td>
-                
-            //     <i onClick={()=>{this.upItem(item)}}  class="fa fa-arrow-circle-up	 fa-3x"></i>
-            //     <i onClick={()=>{this.downItem(item)}}  class="fa fa-arrow-circle-down fa-3x"></i>
-            //     <i onClick={() => this.leftItem(item)}  class="fa fa-arrow-circle-left fa-3x"></i>
-            //     <i onClick={() => this.rightItem(item)}  class="fa fa-arrow-circle-right fa-3x"></i>
-            //     <i onClick={() => this.deleteItem(item)} class="fa fa-trash-o fa-3x"></i>
-                
-            //   </td>
-            //   <td className={ `list-item ${this.state.parent ? "shiftInside" : ""} ${this.state.child ? "shiftDeepInside" : ""}` }> {item} </td>
-            // </tr>
              ))}
 
               <tr>
